a64991539acbaeff0b40067238c8318d7af076d0
LukasDano <lukas.danowski@mail.de>
2025-03-27 19:30:03 +0100

    basic input-output

---
+++ Backend/src/main/java/com/example/Chatbot/basic/BIOS.java
@@ -0,0 +1,41 @@
+package com.example.Chatbot.basic;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/input")
+public class BIOS {
+
+    private String answer;
+
+    public String getAnswer(){
+        return answer;
+    }
+
+    public void setAnswer(String answer){
+        this.answer = answer;
+    }
+
+    @GetMapping("/basic")
+    @CrossOrigin(origins = "http://localhost:5173")
+    public String answerFrontend(@RequestParam String data){
+
+        BIOS datenVerarbeitung = new BIOS();
+        datenVerarbeitung.workWithInput(data);
+
+        return datenVerarbeitung.getAnswer();
+    }
+
+    private void workWithInput(String data){
+
+        String willkommen = "hallo";
+        String welcome = "hello";
+
+        if (data.toLowerCase().equals(willkommen) || data.toLowerCase().equals(welcome)){
+            setAnswer("Hallo auch!");
+        } else {
+            setAnswer("Nochmal bitte!");
+        }
+    }
+
+}
