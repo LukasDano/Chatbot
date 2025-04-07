3df384a067fc2e683602210c08442e39cb7b8b56
LukasDano <lukas.danowski@mail.de>
2025-04-07 13:14:21 +0200

    better structure for Bot answers

--- Backend/src/main/java/com/example/Chatbot/textadventure/Bot.java
+++ Backend/src/main/java/com/example/Chatbot/textadventure/Bot.java
@@ -1,7 +1,13 @@
 package com.example.Chatbot.textadventure;
 
+import org.json.JSONObject;
+
+import java.util.Objects;
+import java.util.ServiceLoader;
+
 public class Bot {
     private String answer;
+    private String category;
 
     private final String EMAIL = ServiceCategories.EMAIL.getValue().toLowerCase();
     private final String DRUCKER = ServiceCategories.DRUCKER.getValue().toLowerCase();
@@ -22,47 +28,73 @@ public class Bot {
         this.answer = answer;
     }
 
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    //TODO gibt ein Objekt mit der Kategorie und der Antwort ins Frontend
+    public void getResult(){
+        JSONObject result = new JSONObject();
+    }
+
     public void workWithInput(String data) {
 
         String inputData = data.toLowerCase();
+        chooseCategory(inputData);
+
+        switch (category){
+            case "email":
+                if (inputData.equals(TICKET)) {
+                    setAnswer("Seit wann besteht ihr Problem");
+                } else if (inputData.equals(EXIT)) {
+                    setAnswer("Schön das Sie ihr Problem lösen konnten, ich wünsche einen schönen Tag!");
+                } else {
+                    setAnswer("Ihre Eingabe ist mir unbekannt!");
+                }
+                break;
+        }
+    }
+
+    private void chooseCategory(String inputData) {
 
         if (inputData.equals(EMAIL)) {
-            setAnswer("Ich helfe dir mit deinem Email Problem! Hier findest du unsere Wissendatenbank zu deiner Problemkategorie. Sollte für dich keine Lösung vorhanden können wir ein Ticket erstellen. Tippe Ticket um ein Ticket zu erstellen und Exit um den Chat zu verlassen.");
-            if (inputData.equals(TICKET)){
-                setAnswer("Seit wann besteht ihr Problem");
-            }
-            else if(inputData.equals(EXIT)) {
-                setAnswer( "Schön das Sie ihr Problem lösen konnten, ich wünsche einen schönen Tag!");
-            }
-            else {
-                setAnswer( "Ihre Eingabe ist mir unbekannt!");
-            }
+            setAnswer("Ich helfe dir mit deinem Email Problem! Hier findest du unsere Wissendatenbank zu deiner Problemkategorie. " +
+                    "Sollte für dich keine Lösung vorhanden können wir ein Ticket erstellen. Tippe Ticket um ein Ticket zu erstellen und Exit um den Chat zu verlassen.");
+            setCategory(EMAIL);
+
         } else if (inputData.equals(DRUCKER)) {
             setAnswer("Ich helfe dir mit deinem Drucker Problem!");
-        }
-        else if (inputData.equals(NETZWERK)) {
+            setCategory(DRUCKER);
+
+        } else if (inputData.equals(NETZWERK)) {
             setAnswer("Ich helfe dir mit deinem Netzwerk Problem!");
-        }
-        else if (inputData.equals(ZUGRIFF)) {
+            setCategory(NETZWERK);
+
+        } else if (inputData.equals(ZUGRIFF)) {
             setAnswer("Ich helfe dir mit deinem Zugriffs Problem!");
-        }
-        else if (inputData.equals(DATENSICHERUNG)) {
+            setCategory(ZUGRIFF);
+
+        } else if (inputData.equals(DATENSICHERUNG)) {
             setAnswer("Ich helfe dir mit deinem Datensicherungs Problem!");
-        }
-        else if (inputData.equals(PERIPHERIE)) {
+            setCategory(DATENSICHERUNG);
+
+        } else if (inputData.equals(PERIPHERIE)) {
             setAnswer("Ich helfe dir mit deinem Peripherie Problem!");
-        }
-        else if (inputData.equals(SOFTWARE)) {
+            setCategory(PERIPHERIE);
+
+        } else if (inputData.equals(SOFTWARE)) {
             setAnswer("Ich helfe dir mit deinem Software Problem!");
-        }
-        else if (inputData.equals(ANDERE)) {
+            setCategory(SOFTWARE);
+
+        } else if (inputData.equals(ANDERE)) {
             setAnswer("Ich helfe dir mit deinen anderen Problemen!");
-        }
-        else {
+            setCategory(ANDERE);
+
+        } else {
             setAnswer("Nochmal bitte!");
         }
-
     }
+
 }
 
 
